106bac12b5772f4ae759a038170ea9afbeca579e
mullenslucas <mullenlucas@gmail.com>
2022-08-29 17:32:20 -0300

    file contruction

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+/* eslint-disable */
 import './style.css';
+
+const fetchid = async () => {
+  let response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/', {
+    method: 'POST',
+  })
+  console.log(response)
+};
+
+fetchid()
