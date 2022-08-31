6b43560aa104ce850ee65ce51ec0da3d09346abc
mullenslucas <mullenlucas@gmail.com>
2022-08-31 11:25:41 -0300

    remove unnecessary comments and fix linters

--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
-/* eslint-disable */
 import './style.css';
 import './cardStyles.css';
-import Series from './modules/Series';
+import Series from './modules/Series.js';
 import createCard from './cards.js';
 
 // Initialize Series Class
 const seriesCl = new Series();
 
-let baseUrl = 'https://api.tvmaze.com/shows'
+const baseUrl = 'https://api.tvmaze.com/shows';
 
 const fetchShows = async () => {
-  let response = await fetch(baseUrl)
-  let data = await response.json()
- 
-  data.splice(141).forEach( (el, i) => {
-    seriesCl.allSeries.push(el)
-    console.log(seriesCl.allSeries[i])
-    console.log(seriesCl.allSeries[i].image.medium)
-    createCard(i, seriesCl.allSeries[i].name, seriesCl.allSeries[i].image.medium)
-  })
+  const response = await fetch(baseUrl);
+  const data = await response.json();
+
+  data.splice(141).forEach((el, i) => {
+    seriesCl.allSeries.push(el);
+    createCard(i, seriesCl.allSeries[i].name, seriesCl.allSeries[i].image.medium);
+  });
 };
 
-fetchShows()
+fetchShows();
 
 // const fetchid = async () => {
 //   let response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/', {
