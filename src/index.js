c5af5ded05a63cfb45358eff33445a905ec7a396
Efiamotu-1 <musahabeebefy10@gmail.com>
2022-08-31 09:52:17 +0100

    linters checks

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './popup.css';
 import './cardStyles.css';
 import Series from './modules/Series';
 import createCard from './cards.js';
+import { openModal } from './popup';
 
 // Initialize Series Class
 const seriesCl = new Series();
@@ -16,8 +17,8 @@ const fetchShows = async () => {
  
   data.splice(141).forEach( (el, i) => {
     seriesCl.allSeries.push(el)
-    console.log(seriesCl.allSeries[i])
-    console.log(seriesCl.allSeries[i].image.medium)
+    // console.log(seriesCl.allSeries[i])
+    // console.log(seriesCl.allSeries[i].image.medium)
     createCard(i, seriesCl.allSeries[i].name, seriesCl.allSeries[i].image.medium)
   })
 };
@@ -31,3 +32,19 @@ fetchShows()
 //   console.log(response)
 // };
 // fetchid()
+
+const displayPop = async() => {
+  await fetchShows()
+  let popup = document.querySelectorAll('.comment-btn')
+  let modal = document.querySelector('.modal')
+  popup.forEach(el => {
+      el.addEventListener('click', (e) => {
+        let id = Number(e.target.id.split('d')[1])
+        modal.classList.toggle('hide')       
+       openModal(id)
+      })
+    })
+
+}
+
+displayPop()
--- src/popup.js
+++ src/popup.js
@@ -1,15 +1,14 @@
-console.log('working')
-
-let baseUl = 'https://api.tvmaze.com/shows'
+const baseUl = 'https://api.tvmaze.com/shows';
 const fetchShow = async () => {
-  let response = await fetch(baseUl)
-  let data = await response.json()
-  return data
+  const response = await fetch(baseUl);
+  let data = await response.json();
+  data = data.splice(141);
+  return data;
 };
 
-export const openModal = async(id) => {
-  let shows = await fetchShow() 
-  let modal = document.querySelector('.modal')
+const openModal = async (id) => {
+  const shows = await fetchShow();
+  const modal = document.querySelector('.modal');
   modal.innerHTML = ` 
   <div class="popup">
   <p id="close" class="close-popup">&times;</p>
@@ -20,7 +19,7 @@ export const openModal = async(id) => {
     <p class="series-title">Name: ${shows[id].name}</p>
     <p class="series-length">Length: ${shows[id].averageRuntime} hours</p>
     <p class="series-length">Premiered: ${shows[id].premiered}</p>
-    <p class="series-length">Ended: ${shows[id].ended ? shows[id].ended : 'still showing' }</p>
+    <p class="series-length">Ended: ${shows[id].ended ? shows[id].ended : 'still showing'}</p>
     <p class="series-length">Language: ${shows[id].language}</p>
     <p class="series-length">Ratings: ${shows[id].rating.average ? shows[id].rating.average : 'not available'}</p>
   </div>
@@ -31,12 +30,13 @@ export const openModal = async(id) => {
       <h3 class="summary">Synopsis</h3>
       <p class="series-desc">${shows[id].summary.replaceAll(/<b>|<\/b>|<p>|<\/p>/g, '')}</p>
       </div>
-      `
-      const close = document.querySelector('.close-popup')
-      close.addEventListener('click', () => {
-      let modal = document.querySelector('.modal')
-      modal.classList.toggle('hide') 
-      modal.innerHTML = ''
+      `;
+  const close = document.querySelector('.close-popup');
+  close.addEventListener('click', () => {
+    const modal = document.querySelector('.modal');
+    modal.classList.toggle('hide');
+    modal.innerHTML = '';
+  });
+};
 
-      })
-  }
+export default openModal;
