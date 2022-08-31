bf3d3a4f302a0fb1c5550397b49957ac982cb502
Efiamotu-1 <musahabeebefy10@gmail.com>
2022-08-31 15:51:58 +0100

    fix linters

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './popup.css';
 import './cardStyles.css';
 import Series from './modules/Series.js';
 import createCard from './cards.js';
-import openModal  from './popup';
+import openModal from './popup.js';
 
 // Initialize Series Class
 const seriesCl = new Series();
@@ -11,13 +11,13 @@ const seriesCl = new Series();
 const baseUrl = 'https://api.tvmaze.com/shows';
 
 const fetchShows = async () => {
-  let response = await fetch(baseUrl)
-  let data = await response.json()
- 
-  data.splice(141).forEach( (el, i) => {
-    seriesCl.allSeries.push(el)
-    createCard(i, seriesCl.allSeries[i].name, seriesCl.allSeries[i].image.medium)
-  })
+  const response = await fetch(baseUrl);
+  const data = await response.json();
+
+  data.splice(141).forEach((el, i) => {
+    seriesCl.allSeries.push(el);
+    createCard(i, seriesCl.allSeries[i].name, seriesCl.allSeries[i].image.medium);
+  });
 };
 
 fetchShows();
@@ -30,19 +30,17 @@ fetchShows();
 // };
 // fetchid()
 
-const displayPop = async() => {
-  await fetchShows()
-  let popup = document.querySelectorAll('.comment-btn')
-  let modal = document.querySelector('.modal')
-  popup.forEach(el => {
-      el.addEventListener('click', (e) => {
-        let id = Number(e.target.id.split('d')[1])
-        modal.classList.toggle('hide')       
-       openModal(id)
-       
-      })
-    })
+const displayPop = async () => {
+  await fetchShows();
+  const popup = document.querySelectorAll('.comment-btn');
+  const modal = document.querySelector('.modal');
+  popup.forEach((el) => {
+    el.addEventListener('click', (e) => {
+      const id = Number(e.target.id.split('d')[1]);
+      modal.classList.toggle('hide');
+      openModal(id);
+    });
+  });
+};
 
-}
-
-displayPop()
+displayPop();
