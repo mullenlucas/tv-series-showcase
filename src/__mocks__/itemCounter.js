3c7425efa5c854a6f57f4b17ae3688e762f87d4f
mullenslucas <mullenlucas@gmail.com>
2022-09-01 20:51:17 -0300

    fix linters

--- src/__mocks__/itemCounter.js
+++ src/__mocks__/itemCounter.js
@@ -2,7 +2,7 @@ const itemCounter = (ori, arr) => {
   ori.splice(141).forEach((el) => {
     arr.push(el);
   });
-  return arr.length
+  return arr.length;
 };
 
 export default itemCounter;
--- src/itemCounter.test.js
+++ src/itemCounter.test.js
@@ -1,13 +1,13 @@
+import fetch from 'node-fetch';
 import itemCounter from './__mocks__/itemCounter.js';
-import fetch from "node-fetch";
 
 describe('Test the amount of items displayed', () => {
   test('Array of items to contain 99 tv-series', async () => {
     const baseUrl = 'https://api.tvmaze.com/shows';
-    let itemsArray = [];
+    const itemsArray = [];
     const response = await fetch(baseUrl);
     const data = await response.json();
-  
+
     expect(itemCounter(data, itemsArray)).toBe(99);
   });
 });
