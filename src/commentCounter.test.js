2d1b6f2e521538cfd1aa140876cc3997ce8f582f
Efiamotu-1 <musahabeebefy10@gmail.com>
2022-09-01 22:32:26 +0100

    add test to check for the comments count.

--- src/commentCounter.test.js
+++ src/commentCounter.test.js
@@ -2,7 +2,11 @@ import commentCounter from './__mocks__/commentCounter.js';
 
 describe('test count of comments made on a particular series', () => {
   test('comments array contains two comments', () => {
-    const comment = ['comment1', 'comment2'];
+    const comment = [
+      { comment: 'good enough series for a drama', creation_date: '2022-08-31', username: 'Ronaldo' },
+      { comment: 'a good series to watch', creation_date: '2022-08-31', username: 'paul' },
+      { comment: 'nice series all round', creation_date: '2022-08-31', username: 'messi' },
+    ];
     expect(commentCounter(comment)).toBe(2);
   });
 
